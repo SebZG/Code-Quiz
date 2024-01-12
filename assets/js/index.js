0f4faf4cfe4337c3414e5b8dc29984440677910d
SebZG <sebastian.zapata.g@gmail.com>
2024-01-12 00:00:05 +0000

    Added rest of JS files + HTML element references

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// Timer
+const time = document.getElementById("time");
+// Start screen
+const startScreen = document.getElementById("start-screen");
+const start = document.getElementById("start");
+// Questions
+const questions = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const choices = document.getElementById("choices");
+// End screen
+const endScreen = document.getElementById("end-screen");
+const finalScore = document.getElementById("final-score");
+const intials = document.getElementById("initials");
+const submit = document.getElementById("submit");
+// Feedback
+const feedback = document.getElementById("feedback");
---
+++ assets/js/scores.js
@@ -0,0 +1,4 @@
+// Highscores
+const highScores = document.getElementById("highscores");
+// Clear
+const clear = document.getElementById("clear");
